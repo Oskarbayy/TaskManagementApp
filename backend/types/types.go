be2014c12c9e06c706f602600cb98dd734dbe1de
Oskar Bay Blichsted Hansen <osha@vestergaardcompany.com>
2024-12-12 15:49:10 +0100

    setup database setup api setup testing setup password encode setup env...

--- backend/types/types.go
+++ backend/types/types.go
@@ -1,1 +1,27 @@
 package types
+
+import "time"
+
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(User) error
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	NotesId   int       `json:"notes_id"`
+	Rank      string    `json:"rank"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	LastLogin time.Time `json:"last_login"`
+}
+
+type RegisterUserPayload struct {
+	FirstName string `json:"firstName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
